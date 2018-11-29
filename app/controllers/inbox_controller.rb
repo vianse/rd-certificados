c2788f94ed30784c12d0b0fd2bd09fe86095e45f
Juan Carlos Santiago <krlos013@gmail.com>
2018-11-28 20:10:46 -0600

    3 commit

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -7,11 +7,14 @@ class InboxController < ApplicationController
     cookies[:user_id] = params[:token]
    # crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base)
    # decrypted_back = crypt.decrypt_and_verify(params[:token])
-   cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').encrypt
-   cipher = Digest::SHA1.hexdigest params[:token]
+   #cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').encrypt
+   #cipher = Digest::SHA1.hexdigest params[:token]
+   key = SecureRandom.random_bytes(32)
+   crypt = ActiveSupport::MessageEncryptor.new(key)
+   decrypted_back = crypt.decrypt_and_verify(cookies[:user_id])
     puts cipher
-    @usuarios = Usuario.where(:groupId => params[:grupo]).where(:email=>cipher)
-    @usuario = Usuario.where(:email=>cipher).pluck(:groupId)
+    @usuarios = Usuario.where(:groupId => params[:grupo]).where(:email=>decrypted_back)
+    @usuario = Usuario.where(:email=>decrypted_back).pluck(:groupId)
     @certificados = Certificado.where(:grupoid => [@usuario])
     @certificado = Certificado.where(:grupoid => params[:grupo])
     respond_to do |format|
