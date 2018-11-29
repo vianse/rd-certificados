f9c4e4134abb594c653488d4029619fcf6c789c1
Juan Carlos Santiago <krlos013@gmail.com>
2018-11-28 20:34:24 -0600

    3 commit

--- app/controllers/inbox_controller.rb
+++ app/controllers/inbox_controller.rb
@@ -4,17 +4,17 @@ class InboxController < ApplicationController
   
 
   def index
-    cookies[:user_id] = params[:token]
+    #cookies[:user_id] = params[:token]
    # crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base)
    # decrypted_back = crypt.decrypt_and_verify(params[:token])
    #cipher = OpenSSL::Cipher.new('DES-EDE3-CBC').encrypt
    #cipher = Digest::SHA1.hexdigest params[:token]
    #key = SecureRandom.random_bytes(32)
-   crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base)
-   decrypted_back = crypt.decrypt_and_verify(params[:token])
-    puts "hola#{decrypted_back}"
-    @usuarios = Usuario.where(:groupId => params[:grupo]).where(:email=>decrypted_back)
-    @usuario = Usuario.where(:email=>decrypted_back).pluck(:groupId)
+   #crypt = ActiveSupport::MessageEncryptor.new(Rails.application.secrets.secret_key_base)
+   #decrypted_back = crypt.decrypt_and_verify(params[:token])
+    #puts "hola#{decrypted_back}"
+    @usuarios = Usuario.where(:groupId => params[:grupo]).where(:email=>cookies[:user_id])
+    @usuario = Usuario.where(:email=>cookies[:user_id]).pluck(:groupId)
     @certificados = Certificado.where(:grupoid => [@usuario])
     @certificado = Certificado.where(:grupoid => params[:grupo])
     respond_to do |format|
